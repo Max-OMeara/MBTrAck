3eb17a937badf86bd5152e5f9eb4071bfd2a74a6
Max O'Meara <mxomeara@bu.edu>
2025-02-16 22:59:01 -0500

    Clean Up

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -19,56 +19,56 @@ public class Main {
         return value >= min && value <= max;
     }
     
-    public static String locationToStation(double latitude, double longitude){ //-71.135604
-        if(isWithinRangeLongitude(longitude, -71.14103911295325, -71.1391021606729)){ // new range
+    public static String locationToStation(double latitude, double longitude){
+        if(isWithinRangeLongitude(longitude, -71.14103911295325, -71.1391021606729)){
             return "Warren";
         }
-        else if(isWithinRangeLongitude(longitude, -71.1391021606729, -71.13591507424903)){ // new range
+        else if(isWithinRangeLongitude(longitude, -71.1391021606729, -71.13591507424903)){ 
             return "Allston";
         }
-        else if(isWithinRangeLongitude(longitude, -71.13591507424903, -71.13281401970175)){ // new range
+        else if(isWithinRangeLongitude(longitude, -71.13591507424903, -71.13281401970175)){
             return "Griggs";
         }
-        else if(isWithinRangeLongitude(longitude, -71.13281401970175, -71.12800849093668)){ // new range
+        else if(isWithinRangeLongitude(longitude, -71.13281401970175, -71.12800849093668)){
             return "Harvard Ave";
         }
-        else if(isWithinRangeLongitude(longitude, -71.12800849093668, -71.12258154210207)){ // new range
+        else if(isWithinRangeLongitude(longitude, -71.12800849093668, -71.12258154210207)){
             return "Packards";
         }
-        else if(isWithinRangeLongitude(longitude, -71.12258154210207, -71.11735404770603)){ // new range
+        else if(isWithinRangeLongitude(longitude, -71.12258154210207, -71.11735404770603)){ 
             return "Babcock";
         }
-        else if(isWithinRangeLongitude(longitude, -71.11735404770603, -71.1103529839365)){ // new range
+        else if(isWithinRangeLongitude(longitude, -71.11735404770603, -71.1103529839365)){ 
             return "Amory";
         }
-        else if(isWithinRangeLongitude(longitude, -71.1103529839365, -71.10583693586047)){ // new range
+        else if(isWithinRangeLongitude(longitude, -71.1103529839365, -71.10583693586047)){ 
             return "BU Central";
         }
-        else if(isWithinRangeLongitude(longitude, -71.10583693586047, -71.10220543356607)){ // new range
+        else if(isWithinRangeLongitude(longitude, -71.10583693586047, -71.10220543356607)){ 
             return "BU East";
         }
-        else if(isWithinRangeLongitude(longitude, -71.10220543356607, -71.09755123833298)){ // new range
+        else if(isWithinRangeLongitude(longitude, -71.10220543356607, -71.09755123833298)){
             return "Blandford";
         }
-        else if(isWithinRangeLongitude(longitude, -71.09755123833298, -71.09157048916403)){ // new range
+        else if(isWithinRangeLongitude(longitude, -71.09755123833298, -71.09157048916403)){
             return "Kenmore";
         }
-        else if(isWithinRangeLongitude(longitude, -71.09157048916403, -71.08283834424013)){ // new range
+        else if(isWithinRangeLongitude(longitude, -71.09157048916403, -71.08283834424013)){
             return "Hynes";
         }
-        else if(isWithinRangeLongitude(longitude, -71.08283834424013, 71.07428387483759)){ // new range
+        else if(isWithinRangeLongitude(longitude, -71.08283834424013, 71.07428387483759)){ 
             return "Copely";
         }
-        else if(isWithinRangeLongitude(longitude, 71.07428387483759, -71.06756822617031)){ // Working
+        else if(isWithinRangeLongitude(longitude, 71.07428387483759, -71.06756822617031)){
             return "Arlington";
         }
-        else if(isWithinRangeLongitude(longitude, -71.06756822617031, -71.06331594392393)){ // new range
+        else if(isWithinRangeLongitude(longitude, -71.06756822617031, -71.06331594392393)){
             return "Boylston";
         }
-        else if(isWithinRangeLongitude(longitude, -71.06331594392393, -71.06041496108917)){ // new range
+        else if(isWithinRangeLongitude(longitude, -71.06331594392393, -71.06041496108917)){ 
             return "Park";
         }
-        else if(isWithinRangeLongitude(longitude, -71.06041496108917, -71.05825188655594)){ // new range
+        else if(isWithinRangeLongitude(longitude, -71.06041496108917, -71.05825188655594)){ 
             return "Gov";
         }
         else if(longitude < -71.14103911295325){
@@ -83,26 +83,6 @@ public class Main {
         else{
             return "unknown";
         }
-
-        // Lat and Long of every Green Line-B Station
-
-        // 42.359661, -71.059258 // gov
-        // 42.356344, -71.062400 // park
-        // 42.353095, -71.064534 // boylston
-        // 42.351726, -71.071280 // arlington
-        // 42.349986, -71.077724 // copely
-        // 42.347945, -71.087912 // hynes
-        // 42.348844, -71.095606 // kenmore
-        // 42.349219, -71.100012 // blandford
-        // 42.349729, -71.104299 // bu east
-        // 42.350077, -71.107133 // bu central
-        // 42.350986, -71.114764 // amory
-        // 42.351616, -71.119928 // babcock
-        // 42.351731, -71.125323 // pack
-        // 42.350162, -71.131600 // harvard ave
-        // 42.348834, -71.134381 // griggs
-        // 42.348734, -71.138026 // allston
-        // 42.348895, -71.139788 // warren
     }
 
     public static void printLineLocations() {
@@ -133,7 +113,6 @@ public class Main {
                     JSONObject attributes = trainData.getJSONObject("attributes");
                     double latitude = attributes.getDouble("latitude");
                     double longitude = attributes.getDouble("longitude");
-                    // cconvert lat and long to String of stop name
                     String location = locationToStation(latitude,longitude);
                     String routeId = trainData.getJSONObject("relationships").getJSONObject("route").getJSONObject("data").getString("id");
 
